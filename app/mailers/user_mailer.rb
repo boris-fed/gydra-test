fa56cdae956a7ba9e4a45e211430900d8a885941
boris-fed <boris-fed@yandex.ru>
2016-12-01 23:27:56 +0400

    Adding mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: 'boris-fed@yandex.ru'
+  default from: Rails.configuration.x.smtp_from_user
   
    def currency_notify_email(email, subject, text)
     mail(to: email, subject: subject) do |format|
