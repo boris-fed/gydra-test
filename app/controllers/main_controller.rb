3eeed098b004af7dbd7ec5571a7fbdfcc55631ed
boris-fed <boris-fed@yandex.ru>
2016-12-01 22:36:40 +0400

    Add logging

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -17,20 +17,20 @@ class MainController < ApplicationController
   private
   def get_rates_data
     if Rails.configuration.x.use_memcache
-      puts 'get_rates_memcache'
+      logger.info 'get_rates_memcache'
       @rates=Rails.cache.fetch(:rates_cache_data, expires_in: CommonConst::CACHE_LIFE_TIME) do
         get_rates_from_db
       end
     else
-      puts 'get_rates_data'
+      logger.info 'get_rates_data'
       @rates=get_rates_from_db
     end
-    puts 'get_rates_data - end'
+    logger.info 'get_rates_data - end'
   end
   
   private
   def get_rates_from_db
-      puts 'get_rates_from_db'
+      logger.info 'get_rates_from_db'
       rates=CurrencyRate.joins("LEFT OUTER JOIN currency_codes ON currency_rates.from_currency=currency_codes.code")
       .select("currency_rates.*,currency_codes.name as from_currency_name")
       .order(["last_update","from_currency"]).last(2)
@@ -38,7 +38,7 @@ class MainController < ApplicationController
       rates.each do |rate|
         rate.calc_details()
       end 
-      puts 'get_rates_from_db - end'      
+      logger.info 'get_rates_from_db - end'      
       return rates
   end
 end
