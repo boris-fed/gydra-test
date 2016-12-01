2dc2af3cf9975d9ba0c55193f210dafb86a4fb4c
boris-fed <boris-fed@yandex.ru>
2016-12-01 23:00:41 +0400

    Add demo button

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,6 @@ class MainController < ApplicationController
     @notify=Notify.new do |n|
       n.set_defaults
     end
-    TestJob.perform_now
   end
   
   def get_rates
@@ -14,23 +13,24 @@ class MainController < ApplicationController
     end
   end
   
+  def load_rates
+    TestJob.perform_now
+    redirect_to root_path
+  end
+  
   private
   def get_rates_data
     if Rails.configuration.x.use_memcache
-      puts 'get_rates_memcache'
       @rates=Rails.cache.fetch(:rates_cache_data, expires_in: CommonConst::CACHE_LIFE_TIME) do
         get_rates_from_db
       end
     else
-      puts 'get_rates_data'
       @rates=get_rates_from_db
     end
-    puts 'get_rates_data - end'
   end
   
   private
   def get_rates_from_db
-      puts 'get_rates_from_db'
       rates=CurrencyRate.joins("LEFT OUTER JOIN currency_codes ON currency_rates.from_currency=currency_codes.code")
       .select("currency_rates.*,currency_codes.name as from_currency_name")
       .order(["last_update","from_currency"]).last(2)
@@ -38,7 +38,6 @@ class MainController < ApplicationController
       rates.each do |rate|
         rate.calc_details()
       end 
-      puts 'get_rates_from_db - end'      
       return rates
   end
 end
